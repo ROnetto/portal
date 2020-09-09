2590fe438c4107bd4ac0fe2ba94960b609f9f59c
Rodrigo <rodrigo.onetto.cortes@gmail.com>
2020-09-09 20:22:14 -0300

    Removing comments

--- vaccinations/views.py
+++ vaccinations/views.py
@@ -8,8 +8,6 @@ class VaccinationListCreateAPIView(ListCreateAPIView):
     serializer_class = VaccinationSerializer
     queryset = Vaccination.objects.all()
 
-    # permission_classes = [IsAuthenticated]
-
 
 vaccination_list_create_view = VaccinationListCreateAPIView.as_view()
 
@@ -19,8 +17,6 @@ class VaccinationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Vaccination.objects.all()
     lookup_field = 'id'
     lookup_url_kwarg = 'id'
-
-    # permission_classes = [IsAuthenticated]
 
 
 vaccination_retrieve_update_delete_view = VaccinationRetrieveUpdateDestroyAPIView.as_view()
